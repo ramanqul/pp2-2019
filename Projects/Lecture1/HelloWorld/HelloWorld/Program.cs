c123343883dae2b3178fedb69d03403766263a55
Ramanqul <buzaubakov.raman@gmail.com>
2019-06-10 09:10:09 +0600

    lecture 1

---
+++ Projects/Lecture1/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            Console.ReadKey();
+        }
+    }
+}
