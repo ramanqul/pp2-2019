b8579725efeb09778f405bc1ac3702ace1183465
Ramanqul <buzaubakov.raman@gmail.com>
2019-06-18 10:24:37 +0600

    Update

--- Projects/Lecture3/ex/ex5/Program.cs
+++ Projects/Lecture3/ex/ex5/Program.cs
@@ -9,14 +9,35 @@ namespace ex5
 
         public static void PrintInfo(FileSystemInfo fsi, int deep)
         {
+            if (fsi.GetType() == typeof(DirectoryInfo))
+            {
+                DirectoryInfo dirInfo = (DirectoryInfo) fsi; //downcast
 
+                foreach(var f in dirInfo.GetFileSystemInfos())
+                {
+                    PrintInfo(f, deep+1);
+                }
+
+                Console.ForegroundColor = ConsoleColor.White;
+            } else {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+
+            for (int i=0;i<deep;i++)
+            {
+                Console.Write(" ");
+            }
+
+            Console.WriteLine(fsi.Name);
+            
         }
 
         static void Main(string[] args)
         {
-            string filePath = @"D:\Code\kbtu\2019\pp1-2019\week2";
+            string filePath = @"D:\Code\kbtu\2019\pp1-2019";
 
             DirectoryInfo dirInfo = new DirectoryInfo(filePath);
+
             PrintInfo(dirInfo, 0);
         }
     }
