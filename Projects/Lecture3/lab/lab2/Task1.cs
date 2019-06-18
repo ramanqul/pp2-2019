13fafcfc10f495cc51a56cfcaecb52c7e776ea4c
Ramanqul <buzaubakov.raman@gmail.com>
2019-06-18 10:30:15 +0600

    update

--- Projects/Lecture3/lab/lab2/Task1.cs
+++ Projects/Lecture3/lab/lab2/Task1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace pp2.lab2
 {
@@ -8,6 +9,15 @@ namespace pp2.lab2
 		public bool isPalindrome(string filePath) 
 		{
             //TODO: place code here
+            FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+            StreamReader st = new StreamReader(fs);
+
+            string input = st.ReadLine();
+
+            //return true if input string is palindrom
+            //else false
+
+
 			return false;
 		}			
     }
