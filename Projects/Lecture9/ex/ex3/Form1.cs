409d503f730e93dc9dc0d36ed2fa729dc48d9ca1
Ramanqul <buzaubakov.raman@gmail.com>
2019-07-03 11:01:14 +0600

    Lecture9 & lab

---
+++ Projects/Lecture9/ex/ex3/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ex3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+            //Draw Triangle
+            /*
+            GraphicsPath gp = new GraphicsPath();
+            gp.AddLine(0, 0, 100, 100);
+            gp.AddLine(100, 100, 300, 0);
+            gp.AddLine(300, 0, 0, 0);
+
+            SolidBrush blackBrush = new SolidBrush(Color.Black);
+            e.Graphics.FillPath(blackBrush, gp);
+            */
+
+
+            GraphicsPath gp = new GraphicsPath();
+            gp.AddLine(50, 0, 25, 80);
+            gp.AddLine(25, 80, 105, 25);
+            gp.AddLine(105, 25, 5, 25);
+            gp.AddLine(5, 25, 75, 80);
+            gp.AddLine(75, 80, 50, 0);
+            Pen p = new Pen(Color.Red);
+            e.Graphics.DrawPath(p, gp);
+
+
+        }
+    }
+}
---
+++ Projects/Lecture9/ex/ex4/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ex4
+{
+    public partial class Form1 : Form
+    {
+
+        Graphics graphics;
+        Bitmap bitmap;
+
+        private Point mouseBeginPoint;
+        private Point mouseEndPoint;
+        private Pen pen = new Pen(Color.Black);
+        private bool isDrawing = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            graphics = Graphics.FromImage(bitmap);
+
+            graphics.Clear(Color.White);
+
+            pictureBox1.Image = bitmap;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.ImageLocation = openFileDialog1.FileName;
+
+                bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                graphics = Graphics.FromImage(bitmap);
+
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                bitmap.Save(saveFileDialog1.FileName);
+            }
+        }
+
+        private void PictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseBeginPoint = e.Location;
+            isDrawing = true; 
+
+        }
+
+        private void PictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDrawing)
+            {
+                mouseEndPoint = e.Location;
+
+                //draw
+
+                graphics.DrawLine(pen, mouseBeginPoint, mouseEndPoint);
+
+
+                mouseBeginPoint = mouseEndPoint;
+
+                pictureBox1.Refresh();
+            }
+        }
+
+        private void PictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDrawing = false;
+        }
+    }
+}
